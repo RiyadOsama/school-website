68c65e4ba3f7cd0137a6f87cc428be0ea3f277ef
asmaaabdelrazek <asmaa.abdelrazek15301@gmail.com>
2025-08-18 09:26:46 +0300

    Added the FAQS Section

--- js/main.js
+++ js/main.js
@@ -8,3 +8,46 @@ $("#btn").click(function () {
   $(".options").toggleClass("show-options");
 });
 // End Header
+
+// Start Header
+// Toggle Active Class To The Navbar
+$(".button").click(function () {
+  $(".button").removeClass("active");
+  $(this).toggleClass("active");
+});
+$("#btn").click(function () {
+  $(".options").toggleClass("show-options");
+});
+// End Header
+
+
+
+
+/****************Start FAQs Section***********************/
+const buttons = document.querySelectorAll(".displayAnswer");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const item = btn.closest(".item");
+    const answer = item.querySelector(".answer");
+
+  
+    document.querySelectorAll(".item").forEach((el) => {
+      if (el !== item) {
+        el.classList.remove("colorWhite");
+        el.querySelector(".answer").classList.add("hide");
+        el.querySelector(".displayAnswer").textContent = "+";
+      }
+    });
+
+    
+    const isHidden = answer.classList.contains("hide");
+    answer.classList.toggle("hide");
+    item.classList.toggle("colorWhite", isHidden);
+    btn.textContent = isHidden ? "-" : "+";
+  });
+});
+
+/****************end FAQs Section***********************/
+
+
