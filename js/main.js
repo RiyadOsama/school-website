1bcc2970ed0656801ad850094b360d17e669b3e2
RiyadOsama <riyadosama14112000@gmail.com>
2025-08-18 22:54:39 +0300

    Added The Testimonials Section

--- js/main.js
+++ js/main.js
@@ -9,45 +9,26 @@ $("#btn").click(function () {
 });
 // End Header
 
-// Start Header
-// Toggle Active Class To The Navbar
-$(".button").click(function () {
-  $(".button").removeClass("active");
-  $(this).toggleClass("active");
-});
-$("#btn").click(function () {
-  $(".options").toggleClass("show-options");
-});
-// End Header
-
-
-
-
-/****************Start FAQs Section***********************/
+// Start FAQs
 const buttons = document.querySelectorAll(".displayAnswer");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
-    const item = btn.closest(".item");
-    const answer = item.querySelector(".answer");
+    const row = btn.closest(".row");
+    const answer = row.querySelector(".answer");
 
-  
-    document.querySelectorAll(".item").forEach((el) => {
-      if (el !== item) {
+    document.querySelectorAll(".row").forEach((el) => {
+      if (el !== row) {
         el.classList.remove("colorWhite");
         el.querySelector(".answer").classList.add("hide");
         el.querySelector(".displayAnswer").textContent = "+";
       }
     });
 
-    
     const isHidden = answer.classList.contains("hide");
     answer.classList.toggle("hide");
-    item.classList.toggle("colorWhite", isHidden);
+    row.classList.toggle("colorWhite", isHidden);
     btn.textContent = isHidden ? "-" : "+";
   });
 });
-
-/****************end FAQs Section***********************/
-
-
+// End FAQs
